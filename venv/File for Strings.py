cbc09eab705a351239661b756a9a7e95e0066216
pranjalyadav <pran.261@gmail.com>
2022-07-18 12:38:33 +0100

    commiting my first change

---
+++ venv/File for Strings.py
@@ -0,0 +1,17 @@
+sentence = "I'm doing fine"
+print(sentence)
+address = '57,Toronto Terrace \nBrighton-BN29UW \nUnited Kingdom'
+print(address)
+
+print('Index p  osition')
+DOB = '26/01/1992'
+print(DOB[6:11])
+print('how to skip every 2nd digit')
+print(DOB[0:11])
+print(type(DOB))
+print(DOB[0:11:2])
+
+sen = sentence.upper()
+print(sen)
+
+print(address.startswith("5"))
